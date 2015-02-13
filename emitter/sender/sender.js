2d797bcbb4efd41ca6925423f7110ac4fdde16a1
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-02-13 19:03:43 +0000

    Handle trigger in own function

--- emitter/sender/sender.js
+++ emitter/sender/sender.js
@@ -5,10 +5,9 @@ var configPath = path.join(__dirname, '..', '..', 'shared', 'config.json'),
     config = require(configPath),
     client = new faye.Client( fayeUrl(config.collector) );
 
-client.subscribe('/trigger', function () {
-  console.log('Trigger message received');
-});
+client.subscribe('/trigger', handleTrigger);
 
+// Send a heartbeat every few secs (defined in config)
 setInterval(heartbeat, config.heartbeatIntervalSecs * 1000);
 
 // Tell everyone we're here
@@ -18,4 +17,8 @@ function heartbeat() {
 
 function fayeUrl(url) {
   return 'http://' + url.host + ':' + url.port + '/faye';
+}
+
+function handleTrigger() {
+  console.log('Trigger message received');
 }
