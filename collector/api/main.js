34c32292bdf5a82e691b975b8f9c27ef83d75df3
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-02-13 17:16:35 +0000

    Placeholder for data collection endpoints

--- collector/api/main.js
+++ collector/api/main.js
@@ -15,6 +15,14 @@ app.get('/status', function (req, res) {
   res.send('UP');
 });
 
+app.post('/metadata', function (req, res) {
+  res.sendStatus(202);
+});
+
+app.post('/image', function (req, res) {
+  res.sendStatus(202);
+});
+
 var server = app.listen(port, function () {
   var host = server.address().address,
       port = server.address().port;
