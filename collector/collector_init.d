9022e5c8eed0b683109e36d424b8f7911026845d
Libby Miller <libby@nicecupoftea.org>
2015-02-24 13:04:49 +0000

    Collector init.d, untested

---
+++ collector/collector_init.d
@@ -0,0 +1,37 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          sender
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Collector
+# Description:       Runs a server and faye to collect data and send it for display
+### END INIT INFO
+# Quick start-stop-daemon example, derived from Debian /etc/init.d/ssh
+set -e
+
+export PATH=$PATH:/usr/local/bin:/opt/node/bin
+export LOG_LEVEL=debug
+
+NAME=collector
+
+export PATH="${PATH:+$PATH:}/usr/sbin:/sbin"
+
+case "$1" in
+  start)
+    echo -n "Starting: "$NAME
+    /opt/node/bin/node /home/pi/whe/collector/api/main.js > /var/log/$NAME_start.log 2>&1
+    echo "."
+    ;;
+  stop)
+    echo -n "Stopping: "$NAME
+    killall node
+    echo "."
+    ;;
+  *)
+    echo "Usage: "$1" {start|stop}"
+    exit 1
+esac
+
+exit 0
