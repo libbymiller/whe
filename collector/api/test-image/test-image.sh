58bcb1f30793a7de5ce79cb76b34e16ac566a181
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-03-03 15:41:41 +0000

    Actual dummy images

--- collector/api/test-image/test-image.sh
+++ collector/api/test-image/test-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 number=$RANDOM
-let "number %= 3"
+let "number %= 2"
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 curl http://127.0.0.1:3000/image -F my_file=@"$dir/image$number.jpg" -F "name=1423085821" -F "source=moz$number"
