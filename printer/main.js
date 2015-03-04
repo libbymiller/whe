76dc1f434eec3a356324cc926397eaadebcd58bc
libby Miller <libby@nicecupoftea.org>
2015-03-04 14:38:25 +0000

    smaller smount of printing, for QCon

--- printer/main.js
+++ printer/main.js
@@ -166,13 +166,37 @@ function imageToAscii(obj) {
 }
 
 function writeFile(params) {
-  var contents = JSON.stringify(params.msg.metadata),
-    obj = params;
-  var header = 'MOZFEST 2014\n*** YOUR SOUVENIR FROM THE ETHICAL DILEMMA CAFE ****\n\n';
+//  var contents = JSON.stringify(params.msg.metadata),
+  var obj = params;
+
+  var metadata_structure = params.msg.metadata;
+  var source = metadata_structure.source;
+  var aps = metadata_structure.aps;
+  var id = metadata_structure.id;
+  var power = metadata_structure.power;
+  var shortName = metadata_structure.shortName;
+  var time = metadata_structure.time;
+  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+  d.setUTCSeconds(parseInt(time));
+
+  var friends = metadata_structure.friends;
+
+  var friends_arr = [];
+  for(var i=0; i<friends.length; i++){
+    var friend_shortName = friends[i].shortName;
+    if(friend_shortName){
+       friends_arr.push(friend_shortName);
+    }
+  }
+
+  var header = '----------------------------------------------------------------------------------\n\n';
+  var contents = d+'\n\nPerson located:\n\n' + shortName + 'user, tracking identifier '+id+'\n\nKnown networks '+aps+'\n\nKnown associates '+friends_arr.join(",")+'\n\n';
 
   console.log('Construct print file');
+  console.log(header + contents);
   return new Promise(function (resolve, reject) {
-    obj.printFile = header + contents + '\n\n\n\n' + obj.ascii;
+//    obj.printFile = header + contents + '\n\n\n\n' + obj.ascii;
+    obj.printFile = header + contents;
     resolve(obj);
   });
 }
