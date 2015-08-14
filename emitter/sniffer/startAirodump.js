08e62c3b59ef1e97c9ed19e07a854015f7910242
Libby Miller <libby@nicecupoftea.org>
2015-08-14 14:09:49 +0000

    Making it work with supervisor

--- emitter/sniffer/startAirodump.js
+++ emitter/sniffer/startAirodump.js
@@ -6,14 +6,20 @@ var configPath = path.join(__dirname, '..', '..', 'shared', 'config.json'),
 
 var dataCsvPrefix = config.sniffer.dataCsvPrefix;
 
+console.log("dataCsvPrefix");
+console.log(dataCsvPrefix);
+
 if (!dataCsvPrefix) {
   console.error('Set config.sniffer.dataCsvPrefix to path to save csv');
   process.exit();
 }
 
 
+
+
 // kill any old airodump processes
-exec('sudo killall airodump-ng', function callback(error, stdout, stderr){
+try {
+  exec('sudo killall airodump-ng', function callback(error, stdout, stderr){
    console.log("sudo killall airodump-ng");
    if (error) {
      console.log(error.stack);
@@ -22,10 +28,16 @@ exec('sudo killall airodump-ng', function callback(error, stdout, stderr){
    }
    console.log('Child Process STDOUT: '+stdout);
    console.log('Child Process STDERR: '+stderr);
-});
+  });
+} catch (e) {
+   console.error('error: sudo killall airodump-ng');
+}
+
 
 //remove old file(s)
-exec('sudo rm '+dataCsvPrefix+'*', function callback(error, stdout, stderr){
+
+try {
+ exec('sudo rm '+dataCsvPrefix+'*', function callback(error, stdout, stderr){
    console.log('sudo rm '+dataCsvPrefix+'*');
    if (error) {
      console.log(error.stack);
@@ -34,11 +46,15 @@ exec('sudo rm '+dataCsvPrefix+'*', function callback(error, stdout, stderr){
    }
    console.log('Child Process STDOUT: '+stdout);
    console.log('Child Process STDERR: '+stderr);
-});
+ });
+} catch (e) {
+   console.error('error: sudo killall airodump-ng');
+}
 
 
 // start up airmon and airodump
-exec('sudo airmon-ng start wlan1', function callback(error, stdout, stderr){
+try {
+ exec('sudo airmon-ng start wlan1', function callback(error, stdout, stderr){
    console.log("sudo airmon-ng start wlan1");
    if (error) {
      console.log(error.stack);
@@ -47,19 +63,30 @@ exec('sudo airmon-ng start wlan1', function callback(error, stdout, stderr){
    }else{
      console.log("success");
      //dump to csv file
-     exec('nohup sudo airodump-ng mon0 --channel 11 --berlin 2 --output-format csv --write '+dataCsvPrefix+' > /dev/null 2>&1 &', function callback(error, stdout, stderr){
-       console.log('sudo airodump-ng mon0 --channel 11 --berlin 2 --output-format csv --write '+dataCsvPrefix);
-       if (error) {
-         console.log(error.stack);
-         console.log('Error code: '+error.code);
-         console.log('Signal received: '+error.signal);
-       }
-       console.log('Child Process STDOUT: '+stdout);
-       console.log('Child Process STDERR: '+stderr);
-     });
+     try{
+
+       exec('nohup sudo airodump-ng mon0 --channel 11 --berlin 2 --output-format csv --write '+dataCsvPrefix+' > /tmp/airlog.txt 2>&1 &', function callback(error, stdout, stderr){
+
+         if (error) {
+           console.log(error.stack);
+           console.log('Error code: '+error.code);
+           console.log('Signal received: '+error.signal);
+         }
+         console.log('Child Process STDOUT: '+stdout);
+         console.log('Child Process STDERR: '+stderr);
+       });
+      } catch (e) {
+        console.error('error: sudo airodump-ng mon0');
+      }
    }
    console.log('Child Process STDOUT: '+stdout);
    console.log('Child Process STDERR: '+stderr);
-});
+
+   // keep alive
+   process.stdin.resume();
+ });
+} catch (e) {
+   console.error('error: sudo airmon-ng start wlan1');
+}
 
 
