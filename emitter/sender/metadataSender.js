5bdbadee3b607ea118bc837e4b3a5d7a384d16cb
Libby Miller <libby@nicecupoftea.org>
2015-08-14 16:51:57 +0000

    Generate json from csv file

--- emitter/sender/metadataSender.js
+++ emitter/sender/metadataSender.js
@@ -11,10 +11,14 @@ var configPath = path.join(__dirname, '..', '..', 'shared', 'config.json'),
     heartbeatInfo = utils.heartbeatInfoForType('sniffer'),
     client = new faye.Client( fayeUrl(config.collector) );
 
-var dataFilename = config.sniffer.dataFilename;
+var os = require("os");
+var host = os.hostname();
 
-if (!dataFilename) {
-  console.error('Set config.sniffer.dataFilename to path to save JSON');
+var dataFilename = config.sniffer.dataFilename;
+var dataCsvPrefix = config.sniffer.dataCsvPrefix;
+
+if (!dataFilename || !dataCsvPrefix) {
+  console.error('Set config.sniffer.dataCsvPrefix and config.sniffer.dataFilename to path to save JSON');
   process.exit();
 }
 
@@ -38,7 +42,7 @@ function handleTrigger() {
    try {
       // load file from the expected place if it's not too old
       console.log('looking for creation time of '+path.join(__dirname, dataFilename));
-      fs.stat(dataFilename, fsDateCallback);
+      fs.stat(dataCsvPrefix+"-01.csv", fsDateCallback);
    } catch(e) {
       console.log("problem");
       console.error(e.stack);
@@ -58,11 +62,12 @@ function fsDateCallback(err, data){
      console.log('startDate '+startDate+' endDate '+endDate+' diff '+diff/1000+' threshold '+threshold+' secs');
      if(diff < threshold){
        console.log('metadata data age is ok, sending');
-       fs.readFile(dataFilename, 'utf8', function (err,fdata) {
+       fs.readFile(dataCsvPrefix+"-01.csv", 'utf8', function (err,fdata) {
          if (err) {
            return console.log(err);
          }
-         sendData(fdata);
+         var json = parseCsvToJson(fdata);
+         sendData(JSON.stringify(json));
        });
      }else{
        console.log("metadata is too old - not sending");
@@ -96,5 +101,48 @@ function requestCallback(err, res, body) {
    }
    console.log("ok");
    console.log(body);
+}
+
+
+// parse csv into the format that we want
+function parseCsvToJson(fdata){
+  var lines = fdata.split("\r\n");
+  var sniffed_lines = [];
+  var count = null;
+  for (var i=0; i<lines.length; i++){
+     var components = lines[i].split(",");
+     for (var j=0; j<lines.length; j++){
+       if(components[j]=="Station MAC"){
+         console.log("got first line");
+         sniffed_lines = lines.slice(i+1, lines.length);
+       }
+     }
+  }
+  var results = {};
+  var data = [];
+  var ids = [];
+  for (var i=0; i < sniffed_lines.length; i++){
+    var sniffed_components = sniffed_lines[i].split(",");
+    var item = {};
+    var id = sniffed_components[0];
+
+    if(id && id!=''){
+      var new_id_arr = id.split(":");
+      var new_id = new_id_arr[0]+":"+new_id_arr[1]+":"+new_id_arr[2]+":"+new_id_arr[3]+":XX:XX";
+      if(ids.indexOf(new_id)==-1){
+        item["id"] = new_id;
+        item["source"] = host;
+        item["aps"] = sniffed_components.slice(6,sniffed_components.length).join(",");
+        item["power"] = sniffed_components[3].trim();
+        var d = new Date(sniffed_components[2]);
+        item["time"] = (d.getTime() / 1000).toString();
+        data.push(item);
+        ids.push(new_id);
+      }
+    }
+  }
+  results["data"] = data;
+  console.log(results);
+  return results;
 }
 
