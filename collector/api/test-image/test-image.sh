e603858b7b5cb0213b37e5043a2c73b61a88bfd2
Dan Nuttall <pixelblend@gmail.com>
2015-09-01 14:13:19 +0100

    use port 5000 for consistancy

--- collector/api/test-image/test-image.sh
+++ collector/api/test-image/test-image.sh
@@ -4,4 +4,4 @@ number=$RANDOM
 let "number %= 2"
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-curl http://127.0.0.1:3000/image -F my_file=@"$dir/image$number.jpg" -F "name=1423085821" -F "source=moz$number"
+curl http://127.0.0.1:5000/image -F my_file=@"$dir/image$number.jpg" -F "name=1423085821" -F "source=moz$number"
