6962f391d33d1277a9bd08fea36a2f2fc447cf22
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-09-07 16:32:08 +0100

    Six test images

--- collector/api/test-image/test-image.sh
+++ collector/api/test-image/test-image.sh
@@ -4,4 +4,7 @@ number=$RANDOM
 let "number %= 2"
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-curl http://127.0.0.1:5000/image -F my_file=@"$dir/image$number.jpg" -F "name=1423085821" -F "source=moz$number"
+for number in 1 2 3 4 5 6
+do
+  curl http://127.0.0.1:5000/image -F my_file=@"$dir/image$number.jpg" -F "name=1423085821" -F "source=whe$number"
+done
