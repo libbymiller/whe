5312cf9e840e20952efd001e6d5854d4b980b8ee
Dan Nuttall <pixelblend@gmail.com>
2015-09-07 17:39:18 +0100

    Update script to match available images

--- collector/api/test-image/test-image.sh
+++ collector/api/test-image/test-image.sh
@@ -4,7 +4,7 @@ number=$RANDOM
 let "number %= 2"
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-for number in 1 2 3 4 5 6
+for number in {0..5}
 do
   curl http://127.0.0.1:5000/image -F my_file=@"$dir/image$number.jpg" -F "name=1423085821" -F "source=whe$number"
 done
