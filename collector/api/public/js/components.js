110b12914127f850934f31c96136229d054605c1
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-09-08 11:44:48 +0100

    Re-render image layout when latestImages change

--- collector/api/public/js/components.js
+++ collector/api/public/js/components.js
@@ -1,21 +1,19 @@
+var ImagesComponent = Ractive.extend({
+  template: '#ui-images',
+  oninit: function () {
+    console.log('init');
+    this.root.observe('latestImages', this.randomiseImages);
+    setInterval(this.randomiseImages.bind(this), 5000);
+  },
+  randomiseImages: function () {
+    var img = this.get('latestImages') || [];
+    this.set('randomImages', _.sample(img, 4));
+  }
+});
+
 Ractive.components = {
   View: Ractive.extend({ template: '#ui-view' }),
-  Images: Ractive.extend({
-    template: '#ui-images', oninit: pickRandom
-  }),
+  Images: ImagesComponent,
   Networks: Ractive.extend({ template: '#ui-networks' }),
   Credits: Ractive.extend({ template: '#ui-credits' })
 };
-
-function rnd() {
-  var img = this.get('latestImages') || [];
-
-  this.set('randomImages', _.sample(img, 4));
-}
-
-function pickRandom() {
-  console.log('pick');
-  //this.observe('latestImages', rnd.bind(this));
-  setTimeout(rnd.bind(this), 500);
-  setInterval(rnd.bind(this), 5000);
-}
